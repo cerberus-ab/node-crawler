1af00bd85d5a9c693b61a4e925580a78c62907bd
cerberus-ab <cerberus.ab@mail.ru>
2019-02-04 12:56:25 +0300

    Use minimist for passign argv

--- crawl-cli.js
+++ crawl-cli.js
@@ -1,7 +1,8 @@
+
+const argv = Object.assign({
+    limit: 20 
+}, require('minimist')(process.argv.slice(2)));
+
 const crawl = require('./src/crawl');
 
-// TODO: should be script parameters
-const start = 'https://agilenihilist.org';
-const limit = 10;
-
-crawl(start, limit).then(result => console.log(result));
+crawl(argv.start, argv.limit).then(result => console.log(result));
