4c57c69be8f01e00f498cc79b9a29a8e233e9bb4
cerberus-ab <cerberus.ab@mail.ru>
2019-02-04 12:28:10 +0300

    Collect links map

--- src/crawl.js
+++ src/crawl.js
@@ -4,22 +4,21 @@ const extract = require('./extract');
 
 function crawl(start, limit = 100) {
     let cache = {};
-    let id = 0;
-    
+    let id = 0;  
     let carry = 0;
     let count = 0;
-    
     let pages = [];
+    let links = [];
     
     return new Promise((resolve, reject) => {
-        !function curl(dst) {
-            let hash = uutil.getHash(dst);
-
-            if (hash in cache === false) {
+        !function curl(src, dst) {
+            let dstHash = uutil.getHash(dst);
+            // create a new page if is not presented yet
+            if (dstHash in cache === false) {
                 if (count + 1 > limit) {
                     return;
                 }
-                cache[hash] = ++id;
+                cache[dstHash] = ++id;
                 let page = { id, url: dst };
                 count++;
                 carry++;
@@ -27,7 +26,7 @@ function crawl(start, limit = 100) {
                 fetch(dst)
                     .then(fetched => {
                         page.code = fetched.code;
-                        extract(fetched, dst, start).forEach(ln => curl(ln));
+                        extract(fetched, dst, start).forEach(ln => curl(dst, ln));
                     })
                     .catch(err => {
                         page.code = null;
@@ -35,11 +34,17 @@ function crawl(start, limit = 100) {
                     .finally(() => {
                         pages.push(page);
                         if (--carry === 0) {
-                            resolve({ pages, count, fin: count < limit });
+                            resolve({ pages, links, count, fin: count < limit });
                         }
                     });
             }
-        }(start);
+            // save the link if it is not root
+            if (src !== null) {
+                let srcHash = uutil.getHash(src);
+                links.push({ from: cache[srcHash], to: cache[dstHash] });
+            }
+            
+        }(null, start);
     });
 }
 
